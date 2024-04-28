23de8c8be549a61e07db87d99f7c05df60e9313f
craftybloxy <craftybloxy@gmail.com>
2024-04-28 15:48:52 +0200

    updated start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 term_lines=$(tput lines)
 while [ true ]; do
-    python ./src/mpqueue_cli/main.py --length $term_lines
+    python ./mdqueue-cli/main.py --length $term_lines
     clear
     echo mpd is down! restarting .
     sleep 1
-    python ./src/mpqueue_cli/main.py --length $term_lines
+    python ./mdqueue-cli/main.py --length $term_lines
     clear
     echo mpd is down! restarting ..
     sleep 1
